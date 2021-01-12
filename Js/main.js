7dd2a169b180604279a9db8f739f7af393cea814
Sr.Dark <yagolopesretur@hotmail.com>
2021-01-11 22:42:50 -0400

    comit rt

---
+++ Js/main.js
@@ -0,0 +1,10 @@
+$(".slide-principal").slick({
+    dots: true,
+    infinit: true,
+    speed: 300,
+    slidestoshow: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    centerpadding: "60px"
+
+});
